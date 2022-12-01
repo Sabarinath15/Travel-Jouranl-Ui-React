ef0cdd2f6ff63c74a477e774a76033f77f6f3943
Sabarinath <sabariavi43@gmail.com>
2022-12-01 11:05:00 +0530

    Final Change

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,14 +2,14 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 
 const Tile = (props) => {
-    return(
+    return (
         <div className="tile">
             <div className="img-con">
                 <img src={`../images/${props.detail.imageUrl}`} alt="" />
             </div>
             <div className="info-con">
                 <div className="top">
-                    <p><span><FaMapMarkerAlt/></span>{props.detail.location}</p>
+                    <p><span><FaMapMarkerAlt /></span>{props.detail.location}</p>
                     <a href={`${props.detail.googleMapsUrl}`}>View on Google Maps</a>
                 </div>
                 <h1>{props.detail.title}</h1>
